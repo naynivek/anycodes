02358c9b7c81c6f4e50bdbbe7d96d3ea57acee2c
naynivek <naynivek@users.noreply.github.com>
2017-03-17 14:04:20 -0300

    Add limit 1

--- php/consult-bd.php
+++ php/consult-bd.php
@@ -7,7 +7,7 @@ $user = "user";
 $pass = "pass";
 
 $conecta = mysqli_connect("127.0.0.1", "root", "123456","t") or print (mysqli_error());
-$sql = "SELECT * FROM users AS a WHERE a.nome = '" .$user. "' and a.senha = ".$pass;
+$sql = "SELECT * FROM users AS a WHERE a.nome = '" .$user. "' and a.senha = ".$pass." limit 1";
 $result = mysqli_query($conecta, $sql) or print (mysqli_error());
 $fetch = mysqli_num_rows($result);
 print $fetch;
