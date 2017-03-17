7a09223e5a2d4b3442a98c5e982d0878482b938d
naynivek <naynivek@users.noreply.github.com>
2017-03-17 14:01:58 -0300

    Create consult-bd.php

---
+++ php/consult-bd.php
@@ -0,0 +1,15 @@
+php version 7
+<?php
+
+
+$table = "table";
+$user = "user";
+$pass = "pass";
+
+$conecta = mysqli_connect("127.0.0.1", "root", "123456","t") or print (mysqli_error());
+$sql = "SELECT * FROM users AS a WHERE a.nome = '" .$user. "' and a.senha = ".$pass;
+$result = mysqli_query($conecta, $sql) or print (mysqli_error());
+$fetch = mysqli_num_rows($result);
+print $fetch;
+
+//prints 1 for ok and prints 0 for not ok
